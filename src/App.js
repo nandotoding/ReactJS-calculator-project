13c586b1f58938657ea5e5a52c39138f2eec2afd
nandotoding <nandotodingbua@gmail.com>
2022-12-23 01:02:37 +0700

    modified

--- src/App.js
+++ src/App.js
@@ -3,20 +3,47 @@ import {useState} from "react";
 
 function App() {
     const [number, setNumber] = useState("0");
+    const [expression, setExpression] = useState("");
 
-    const numClickHandler = (e) => {
+    const numClickHandler = e => {
         e.preventDefault();
+        let numVal, expVal;
 
         if (number === "0") {
-            setNumber(e.currentTarget.innerText);
+            numVal = e.currentTarget.innerText;
+            setExpression(expression + numVal);
         } else {
-            setNumber(number + e.currentTarget.innerText);
+            numVal = number + e.currentTarget.innerText;
+            setExpression(expression + e.currentTarget.innerText);
         }
-    }
+        setNumber(numVal);
+    };
+
+    const opClickHandler = e => {
+        if (!(expression === "" || expression.includes("+", expression.length - 1) || expression.includes("-", expression.length - 1))) {
+            setExpression(expression + e.currentTarget.innerText);
+            setNumber("0");
+        }
+    };
+
+    const eqClickHandler = () => {
+        if ((expression.includes("+") || expression.includes("-")) && !(expression === "" || expression.includes("+", expression.length - 1) || expression.includes("-", expression.length - 1))) {
+            let result = eval(expression);
+            const strResult = result.toString();
+            setNumber(strResult);
+            setExpression("");
+            console.log(expression + " = " + strResult);
+        }
+    };
+
+    const ceClickHandler = () => {
+        setNumber("0");
+        setExpression("");
+    };
 
     return (
         <>
-        <div className="flex-container-column card">
+            <div className="flex-container-column card">
             <div className="display">
                 <h1 id="displayNumber">{number}</h1>
             </div>
@@ -24,26 +51,26 @@ function App() {
                 <div className="button" onClick={e => numClickHandler(e)}>7</div>
                 <div className="button" onClick={e => numClickHandler(e)}>8</div>
                 <div className="button" onClick={e => numClickHandler(e)}>9</div>
-                <div className="button negative">+/-</div>
+                <div className="button negative">&#128578;</div>
             </div>
             <div className="flex-container-row">
                 <div className="button" onClick={e => numClickHandler(e)}>4</div>
                 <div className="button" onClick={e => numClickHandler(e)}>5</div>
                 <div className="button" onClick={e => numClickHandler(e)}>6</div>
-                <div className="button operator" >-</div>
+                <div className="button operator" onClick={e => opClickHandler(e)}>-</div>
             </div>
             <div className="flex-container-row">
                 <div className="button" onClick={e => numClickHandler(e)}>1</div>
                 <div className="button" onClick={e => numClickHandler(e)}>2</div>
                 <div className="button" onClick={e => numClickHandler(e)}>3</div>
-                <div className="button operator">+</div>
+                <div className="button operator" onClick={e => opClickHandler(e)}>+</div>
             </div>
             <div className="flex-container-row">
-                <div className="button clear">CE</div>
+                <div className="button clear" onClick={ceClickHandler}>CE</div>
                 <div className="button" onClick={e => numClickHandler(e)}>0</div>
-                <div className="button equals double">=</div>
+                <div className="button equals double" onClick={eqClickHandler}>=</div>
             </div>
-        </div>
+            </div>
         </>
     );
 }
